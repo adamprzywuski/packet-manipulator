62c4d4a9ecda0f73eebe690e33840fdce52a9f54
Adam Przywuski <adamprzywuski98@gmail.com>
2020-04-24 02:27:14 +0200

    Adding function

--- catching_packets.py
+++ catching_packets.py
@@ -1,11 +1,9 @@
-import scapy.all
-import logging
 
 
 from scapy.all import *
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-import re
+
 
 def packet_callback(packet):
     if packet['TCP'].payload:
@@ -14,10 +12,17 @@ def packet_callback(packet):
         #if packet['IP'].dport == 80:
         print("\n{} ----HTTP----> {}:{}:\n{}".format(packet['IP'].src, packet['IP'].dst, packet['IP'].dport, str(bytes(packet['TCP'].payload))))
 
-sniff(filter="tcp", prn=packet_callback, store=0)
+        #print(get_host_for_ip(packet['IP'].src))
+        sniff(filter="tcp", prn=packet_callback, store=0)
+
+#allows to exchange ip to http adress
+def get_host_for_ip(ip):
+    try:
+        host=socket.gethostbyaddr(ip)
+    except socket.error:
+        return ip
+    return host
 
 if __name__ == '__main__':
     print("program wlaczony")
     sniff(filter="tcp", prn=packet_callback, store=0)
-
-
