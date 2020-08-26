557e4c15881630f0ab8206cb22d07adcd1897f04
Artur Balczynski <artur.balczynski@student.put.poznan.pl>
2020-08-26 21:59:21 +0200

    playing with sniff

--- catching_packets.py
+++ catching_packets.py
@@ -1,6 +1,6 @@
-
 from scapy.all import *
 import logging
+
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
@@ -9,7 +9,8 @@ def packet_callback(packet):
         pkt = str(packet['TCP'].payload)
 
         # if packet['IP'].dport == 80:
-        print("\n{} ----HTTP----> {}:{}:\n{}".format(packet['IP'].src, packet['IP'].dst, packet['IP'].dport, str(bytes(packet['TCP'].payload))))
+        print("\n{} ----HTTP----> {}:{}:\n{}".format(packet['IP'].src, packet['IP'].dst, packet['IP'].dport,
+                                                     str(bytes(packet['TCP'].payload))))
 
         # print(get_host_for_ip(packet['IP'].src))
         sniff(filter="tcp", prn=packet_callback, store=0)
@@ -18,11 +19,15 @@ def packet_callback(packet):
 # allows to exchange ip to http adress
 def get_host_for_ip(ip):
     try:
-        host =socket.gethostbyaddr(ip)
+        host = socket.gethostbyaddr(ip)
     except socket.error:
         return ip
     return host
 
+
 if __name__ == '__main__':
     print("program wlaczony")
-    sniff(filter="tcp", prn=packet_callback, store=0)
+    packets = sniff(iface="vEthernet (AWSI-External)", count=10)
+    print(packets[0])
+    summary = packets[0].summary()
+    print(summary)
