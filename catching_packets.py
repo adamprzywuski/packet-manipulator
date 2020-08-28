4786a4bf015f612ae39cfe84c57f8caca95e0197
Adam Przywuski <adamprzywuski98@gmail.com>
2020-08-28 22:07:01 +0200

    Class to manage data to front-end

--- catching_packets.py
+++ catching_packets.py
@@ -2,12 +2,13 @@ from scapy.all import *
 import logging
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from show_packets import Packet
 
 
 def packet_callback(packet):
     if packet['TCP'].payload:
         pkt = str(packet['TCP'].payload)
-
+        print(packet.proto)
         # if packet['IP'].dport == 80:
         print("\n{} ----HTTP----> {}:{}:\n{}".format(packet['IP'].src, packet['IP'].dst, packet['IP'].dport,
                                                      str(bytes(packet['TCP'].payload))))
@@ -27,7 +28,13 @@ def get_host_for_ip(ip):
 
 if __name__ == '__main__':
     print("program wlaczony")
-    packets = sniff(iface="vEthernet (AWSI-External)", count=10)
-    print(packets[0])
-    summary = packets[0].summary()
-    print(summary)
+    packets = sniff(iface=None,count=5)
+    for p in packets:
+
+        a= Packet(p)
+        print(a)
+
+    #summary = packets[0].proto
+    #print(summary)
+    #packet_callback(sniff(filter="tcp", prn=packet_callback, store=0))
+
