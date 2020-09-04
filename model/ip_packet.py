807a08cc8f1a0fe7d8377f0b5e597c8e1ae1ec44
Adam Przywuski <adamprzywuski98@gmail.com>
2020-09-04 15:07:18 +0200

    Displaying names the most popular caught protocols

--- model/ip_packet.py
+++ model/ip_packet.py
@@ -9,13 +9,36 @@ def ip_proto(pkt):
 
 class IpPacket:
     def __init__(self, packet):
+        
         self.time = time.clock()
         self.source = packet['IP'].src
         self.destination = packet['IP'].dst
         self.length = packet['IP'].len
         self.ttl = packet['IP'].ttl
-        # self.info = str(bytes(packet['Raw'].load))
-        self.protocol = ip_proto(packet['IP']).capitalize()
+        self.info = " "
+        self.protocol = ip_proto(packet['IP']).upper()
+        if(self.protocol=='TCP' or self.protocol=='UDP'):
+            self.info = str(bytes(packet[self.protocol].payload))
+            if packet['IP'].dport==20 or packet['IP'].dport==21:
+                self.protocol = "FTP"
+            elif packet['IP'].dport==22:
+                self.protocol = "SSH"
+            elif packet['IP'].dport==23:
+                self.protocol = "Telnet"
+            elif packet['IP'].dport==25:
+                self.protocol = "SMTP"
+            elif packet['IP'].dport==53:
+                self.protocol = "DNS"
+            elif packet['IP'].dport==67 or packet['IP'].dport==68:
+                self.protocol = "DHCP"
+            elif packet['IP'].dport==80:
+                self.protocol = "HTTP"
+            elif packet['IP'].dport == 443:
+                    self.protocol = "HTTPS"
+            else:
+                self.protocol=self.protocol+" : "+str(packet['IP'].dport)
+
+
 
     def __str__(self):
         return "Date: {0}, Source:{1}, Destination:{2}, Length:{3},Protocol:{4}, Info:{5}" \
